88aeba233cac82ab8d22eaaa0c45673130296224
Zane Peckenpaugh <zanepeck01@gmail.com>
2025-06-28 13:37:36 -0400

    Added POST endpoint and handling forequest bodies

--- spring-boot-tutorial/src/main/java/com/zanecodes/SoftwareEngineerController.java
+++ spring-boot-tutorial/src/main/java/com/zanecodes/SoftwareEngineerController.java
@@ -3,7 +3,10 @@ package com.zanecodes;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,16 @@ public class SoftwareEngineerController {
     public List<SoftwareEngineer> getEngineers() {
         return softwareEngineerService.getAllSoftwareEngineers();
     }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(@PathVariable Integer id) {
+        return softwareEngineerService.getSoftwareEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
+    
 
 }
